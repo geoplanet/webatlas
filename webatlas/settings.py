0fd48987624e7b60e763ae952fcf57274944cf98
OldCrow <vuntut@DRPCFILES.yt.northwestel.net>
2014-02-16 23:22:40 -0800

    added requirements.txt

--- webatlas/settings.py
+++ webatlas/settings.py
@@ -167,6 +167,10 @@ THUMBNAIL_PROCESSORS = (
     'easy_thumbnails.processors.filters',
 )
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
 SERIALIZATION_MODULES = {
 	'geojson': 'djgeojson.serializers'
 }
